c64843cfe2df1bdcd49f7c8592cc97b0b91adec2
Yana <yanamelnychuk10@gmail.com>
2023-06-20 11:39:15 +0300

    scroll

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import Routes from '../Routes/Routes';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import ScrollToTop from '../App/ScrollTop';
 
 const App = () => {
   return (
     <div className="wrapper">
+        <ScrollToTop />
         <Header />
         <Routes />
         <Footer />
