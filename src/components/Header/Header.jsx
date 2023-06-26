68be78e9119f1477c531daafad40a95e21826d11
Yana <yanamelnychuk10@gmail.com>
2023-06-26 15:34:11 +0300

    header changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ROUTES } from "../../utils/routes";
 
 import styles from "../../styles/Header.module.scss";
@@ -8,6 +8,7 @@ import LOGO from "../../images/logo.jpg";
 import { AiOutlineMenu } from 'react-icons/ai';
 
 const Header = () => {
+  const location = useLocation();
 
   return (
     <header className={`${styles.header}`}>
@@ -19,16 +20,16 @@ const Header = () => {
 
       <div className={styles.header__links_container}>
         <nav className={styles.header__links}>
-          <Link to={ROUTES.HOME} className={styles.headerItem}>Home</Link>
-          <Link to={ROUTES.IMPRINT} className={styles.headerItem}>Impressum</Link>
-          <Link to={ROUTES.CONTACT} className={styles.headerItem}>Kontakt</Link>
-          <Link to={ROUTES.ABOUT} className={styles.headerItem}>Über uns</Link>
-          <Link to={ROUTES.PRESS} className={styles.headerItem}>Presse</Link>
+          <Link to={ROUTES.HOME} className={`${styles.headerItem} ${location.pathname === ROUTES.HOME ? styles.headerItem__activeLink : ''}`}>Home</Link>
+          <Link to={ROUTES.IMPRINT} className={`${styles.headerItem} ${location.pathname === ROUTES.IMPRINT ? styles.headerItem__activeLink : ''}`}>Impressum</Link>
+          <Link to={ROUTES.CONTACT} className={`${styles.headerItem} ${location.pathname === ROUTES.CONTACT ? styles.headerItem__activeLink : ''}`}>Kontakt</Link>
+          <Link to={ROUTES.ABOUT} className={`${styles.headerItem} ${location.pathname === ROUTES.ABOUT ? styles.headerItem__activeLink : ''}`}>Über uns</Link>
+          <Link to={ROUTES.PRESS} className={`${styles.headerItem} ${location.pathname === ROUTES.PRESS ? styles.headerItem__activeLink : ''}`}>Presse</Link>
         </nav>
       </div>
 
       
-      <div className={styles.menu_btn}>
+      <div className={styles.menu__btn}>
         <Link to={ROUTES.MENU} className={styles.menu__link}>
           <AiOutlineMenu size={25} />
         </Link>
